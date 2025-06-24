fc01e4d524efa3dfeb5b64c80da93d1da7d7cf8c
marianakluppell <gomes.mariana24@escola.pr.gov.br>
2025-06-24 12:25:27 +0000

    aula 04

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value;
-    const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ");
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-    campoResultado.textContent = palavras.join(', ');
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
 }
