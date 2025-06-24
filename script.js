2c8c8468682b78730e3357334fffba18fdb89d25
marianakluppell <gomes.mariana24@escola.pr.gov.br>
2025-06-24 13:23:57 +0000

    finalizando aula 6

--- script.js
+++ script.js
@@ -29,3 +29,15 @@ function processaTexto(texto) {
 function contaFrequencias(palavras) {
   let frequencias = {};
 
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  return frequencias;
+}
